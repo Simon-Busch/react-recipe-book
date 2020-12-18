3306073bb8b6c2c00fe5dbd64a726621a4155f14
Simon Busch <simon.busch@live.fr>
2020-12-18 10:55:42 +0100

    image working

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,39 @@
 import React from 'react'
 
 const Card = ({ details }) => {
-
+  const ingredients = details.ingredients
+      .split(',')
+      .map(item => 
+        <li key={item}>{item}</li>)
+  
+  const instructions = details.instructions
+  .split('\n')
+  .map(item => 
+    <li key={item}>{item}</li>)
+  
+  // handle if image path is wrong
+  const requireImage = (chemin) => {
+    try {
+      return require(`../img/${chemin}`)
+    } catch (err) {
+      return require(`../img/default.jpeg`)
+    }
+  }
+      
   return (
     <div className="card">
-      <div className="recettes">
+    <div class="image">
+      {/* {important to mention path with require for when webpack compile} */}
+      <img src={requireImage(details.image)} alt={details.nom} />
+    </div>
+      <div className="recette">
         <h2>{details.nom}</h2>
+        <ul className="liste-ingredients">
+          {ingredients}
+        </ul>
+        <ol className="liste-instructions">
+          {instructions}
+        </ol>
       </div>
     </div>
   )
