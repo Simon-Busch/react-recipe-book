7d1835dd9f04a2f27496c951d3ff96e3630afee5
Simon Busch <simon.busch@live.fr>
2020-12-20 19:25:58 +0100

    redirection

---
+++ build/precache-manifest.dc1d6ff8b5971c2b77b4c102c523ea0b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "00a1d56632699c3cecf9",
+    "url": "/static/css/main.d2a15057.chunk.css"
+  },
+  {
+    "revision": "00a1d56632699c3cecf9",
+    "url": "/static/js/main.00a1d566.chunk.js"
+  },
+  {
+    "revision": "270227abcf1d0d527a2a",
+    "url": "/static/js/1.270227ab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9a86c50cc3fe786eb584dd4dc784caf",
+    "url": "/static/media/default.b9a86c50.jpeg"
+  },
+  {
+    "revision": "4200b6b96b6c171e52c4a97d34a1174e",
+    "url": "/static/media/chocolat.4200b6b9.jpeg"
+  },
+  {
+    "revision": "815852b98cfd976880da9f88ecc5892e",
+    "url": "/static/media/epinard.815852b9.jpg"
+  },
+  {
+    "revision": "fca17319421519cbb9d73c85b4879566",
+    "url": "/static/media/hamburger.fca17319.jpeg"
+  },
+  {
+    "revision": "287d53ae5223bf74627baa68ecc5f5c1",
+    "url": "/static/media/pizza.287d53ae.jpeg"
+  },
+  {
+    "revision": "8324a6142c5fc9d0bdd0b453c162f9a1",
+    "url": "/static/media/saumon.8324a614.jpeg"
+  },
+  {
+    "revision": "96c2f7548bab68d96c489276dbc837e6",
+    "url": "/static/media/tartine.96c2f754.jpg"
+  },
+  {
+    "revision": "35d217b02a6fb074e6e3d1c311ce4de1",
+    "url": "/index.html"
+  }
+];
